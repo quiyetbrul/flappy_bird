ec46c03c323e4bed5e3627883e131708ebb07662
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2022-07-24 22:27:55 -0400

    Update State.h

--- cpp/src/State.h
+++ cpp/src/State.h
@@ -6,7 +6,7 @@ class State {
 public:
   virtual void Init() = 0;
   virtual void HandleInput() = 0;
-  virtual void Update() = 0;
+  virtual void Update(float delta_time) = 0;
   virtual void Draw(float delta_time) = 0;
 
   virtual void Pause(){};
