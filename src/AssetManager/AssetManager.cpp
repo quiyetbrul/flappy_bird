dfb52dc76142b5383bcecc330d05b804e7894521
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2022-08-19 15:18:47 -0400

    Update AssetManager.cpp

--- src/AssetManager/AssetManager.cpp
+++ src/AssetManager/AssetManager.cpp
@@ -7,7 +7,7 @@ void AssetManager::LoadTexture(std::string texture_name,
                                std::string file_name) {
   sf::Texture texture;
   if (!texture.loadFromFile(file_name)) {
-    std::cout << "Failed to load texture!" << std::endl;
+    std::cout << "Failed to load texture: " << texture_name << std::endl;
   } else {
     this->Textures_[texture_name] = texture;
   }
@@ -20,7 +20,7 @@ sf::Texture &AssetManager::GetTexture(std::string texture_name) {
 void AssetManager::LoadFont(std::string texture_name, std::string file_name) {
   sf::Font font;
   if (!font.loadFromFile(file_name)) {
-    std::cout << "Failed to load texture!" << std::endl;
+    std::cout << "Failed to load font: " << texture_name << std::endl;
   } else {
     this->Fonts_[texture_name] = font;
   }
