46a85ccbc55845aaab5387f3f6056a4f98e64e2a
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2022-08-24 08:37:36 -0600

    fix asset pathfiles

--- src/Utils/Definitions.h
+++ src/Utils/Definitions.h
@@ -5,27 +5,27 @@
 #define SCREEN_HEIGHT 1024
 
 #define SPLASH_STATE_SHOW_TIME 0.0
-#define SPLASH_SCENE_BACKGROUND_FILEPATH "../assets/sprites/SplashBackground.png"
+#define SPLASH_SCENE_BACKGROUND_FILEPATH "assets/sprites/SplashBackground.png"
 
-#define MENU_BACKGROUND_FILEPATH "../assets/sprites/sky.png"
-#define GAME_BACKGROUND_FILEPATH "../assets/sprites/sky.png"
-#define GAMEOVER_BACKGROUND_FILEPATH "../assets/sprites/sky.png"
+#define MENU_BACKGROUND_FILEPATH "assets/sprites/sky.png"
+#define GAME_BACKGROUND_FILEPATH "assets/sprites/sky.png"
+#define GAMEOVER_BACKGROUND_FILEPATH "assets/sprites/sky.png"
 
-#define LAND_FILEPATH "../assets/sprites/land.png"
+#define LAND_FILEPATH "assets/sprites/land.png"
 
-#define GAME_TITLE_FILEPATH "../assets/sprites/title.png"
-#define PLAY_BUTTON_FILEPATH "../assets/sprites/PlayButton.png"
+#define GAME_TITLE_FILEPATH "assets/sprites/title.png"
+#define PLAY_BUTTON_FILEPATH "assets/sprites/PlayButton.png"
 
-#define PIPE_UP_FILEPATH "../assets/sprites/PipeUp.png"
-#define PIPE_DOWN_FILEPATH "../assets/sprites/PipeDown.png"
-#define PIPE_SCORING_FILEPATH "../assets/sprites/InvisibleScoringPipe.png"
+#define PIPE_UP_FILEPATH "assets/sprites/PipeUp.png"
+#define PIPE_DOWN_FILEPATH "assets/sprites/PipeDown.png"
+#define PIPE_SCORING_FILEPATH "assets/sprites/InvisibleScoringPipe.png"
 #define PIPE_MOVEMENT_SPEED 200.0f
 #define PIPE_SPAWN_FREQUENCY 2.0f
 
-#define BIRD_FRAME_1_FILEPATH "../assets/sprites/bird-01.png"
-#define BIRD_FRAME_2_FILEPATH "../assets/sprites/bird-02.png"
-#define BIRD_FRAME_3_FILEPATH "../assets/sprites/bird-03.png"
-#define BIRD_FRAME_4_FILEPATH "../assets/sprites/bird-04.png"
+#define BIRD_FRAME_1_FILEPATH "assets/sprites/bird-01.png"
+#define BIRD_FRAME_2_FILEPATH "assets/sprites/bird-02.png"
+#define BIRD_FRAME_3_FILEPATH "assets/sprites/bird-03.png"
+#define BIRD_FRAME_4_FILEPATH "assets/sprites/bird-04.png"
 #define BIRD_ANIMATION_DURATION 0.4f
 #define BIRD_ROTATION_SPEED 100.0f
 #define BIRD_STATE_STILL 1
@@ -37,12 +37,12 @@
 
 #define FLASH_SPEED 1500.0f
 
-#define FLAPPY_FONT_FILEPATH "../assets/fonts/FlappyFont.ttf"
+#define FLAPPY_FONT_FILEPATH "assets/fonts/FlappyFont.ttf"
 
-#define GAME_OVER_TITLE_FILEPATH "../assets/sprites/Game-Over-Title.png"
-#define GAME_OVER_BODY_FILEPATH "../assets/sprites/Game-Over-Body.png"
+#define GAME_OVER_TITLE_FILEPATH "assets/sprites/Game-Over-Title.png"
+#define GAME_OVER_BODY_FILEPATH "assets/sprites/Game-Over-Body.png"
 
-#define HIGHSCORE_FILEPATH "../assets/HighScore.txt"
+#define HIGHSCORE_FILEPATH "assets/HighScore.txt"
 
 enum GameStates { eReady, ePlaying, eGameOver }; // enum GameStates
 
