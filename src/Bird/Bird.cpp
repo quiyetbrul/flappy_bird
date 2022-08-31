5dd42d181b8f46121a355573322633ded1474268
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2022-08-31 07:49:47 -0600

    make bird position values float

--- src/Bird/Bird.cpp
+++ src/Bird/Bird.cpp
@@ -4,7 +4,6 @@ namespace Game {
 Bird::Bird(GameDataRef data)
     : Data_(data), Animation_Iterator_(0), Bird_State_(BIRD_STATE_STILL),
       Rotation_(0.0f) {
-  Animation_Iterator_ = 0;
   Animation_Frames_.push_back(this->Data_->Assets_.GetTexture("Bird Frame 1"));
   Animation_Frames_.push_back(this->Data_->Assets_.GetTexture("Bird Frame 2"));
   Animation_Frames_.push_back(this->Data_->Assets_.GetTexture("Bird Frame 3"));
@@ -12,13 +11,15 @@ Bird::Bird(GameDataRef data)
 
   Bird_Sprite_.setTexture(Animation_Frames_[Animation_Iterator_]);
 
-  Bird_Sprite_.setPosition((this->Data_->Window_.getSize().x / 4.0f) -
-                               (Bird_Sprite_.getGlobalBounds().width / 2.0f),
-                           (this->Data_->Window_.getSize().y / 2.0f) -
-                               (Bird_Sprite_.getGlobalBounds().height / 2.0f));
+  Bird_Sprite_.setPosition(
+      ((float)this->Data_->Window_.getSize().x / 4.0f) -
+          ((float)Bird_Sprite_.getGlobalBounds().width / 2.0f),
+      ((float)this->Data_->Window_.getSize().y / 2.0f) -
+          ((float)Bird_Sprite_.getGlobalBounds().height / 2.0f));
 
-  auto origin = sf::Vector2f(Bird_Sprite_.getGlobalBounds().width / 2.0f,
-                             Bird_Sprite_.getGlobalBounds().height / 2.0f);
+  auto origin =
+      sf::Vector2f((float)Bird_Sprite_.getGlobalBounds().width / 2.0f,
+                   (float)Bird_Sprite_.getGlobalBounds().height / 2.0f);
   Bird_Sprite_.setOrigin(origin);
 }
 
@@ -39,14 +40,14 @@ void Bird::Animate([[maybe_unused]] float delta_time) {
 
 void Bird::Update(float delta_time) {
   if (BIRD_STATE_FALLING == Bird_State_) {
-    Bird_Sprite_.move(0, GRAVITY * delta_time);
+    Bird_Sprite_.move(0.0f, GRAVITY * delta_time);
     Rotation_ += BIRD_ROTATION_SPEED * delta_time;
     if (Rotation_ > 25.0f) {
       Rotation_ = 25.0f;
     }
     Bird_Sprite_.setRotation(Rotation_);
   } else if (BIRD_STATE_FLYING == Bird_State_) {
-    Bird_Sprite_.move(0, -BIRD_FLYING_SPEED * delta_time);
+    Bird_Sprite_.move(0.0f, -BIRD_FLYING_SPEED * delta_time);
     Rotation_ -= BIRD_ROTATION_SPEED * delta_time;
     if (Rotation_ < -25.0f) {
       Rotation_ = -25.0f;
