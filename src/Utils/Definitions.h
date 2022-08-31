43ae47c5635405ad38a1bde276fe0e7fa82832b5
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2022-08-31 15:42:51 -0600

    Update Definitions.h

--- src/Utils/Definitions.h
+++ src/Utils/Definitions.h
@@ -1,54 +1,59 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
-#define GAME_TITLE "Flappy Bird - Quiyet Brul"
+#include <string>
 
-#define SCREEN_WIDTH 768
-#define SCREEN_HEIGHT 1024
+const std::string GAME_TITLE = "Flappy Bird - Quiyet Brul";
 
-#define SPLASH_STATE_SHOW_TIME 0.0f
-#define SPLASH_SCENE_BACKGROUND_FILEPATH "assets/sprites/SplashBackground.png"
+const int SCREEN_WIDTH = 768;
+const int SCREEN_HEIGHT = 1024;
+
+const float SPLASH_STATE_SHOW_TIME = 0.0f;
+const std::string SPLASH_SCENE_BACKGROUND_FILEPATH =
+    "assets/sprites/SplashBackground.png";
 
 #define MENU_BACKGROUND_FILEPATH "assets/sprites/sky.png"
-#define GAME_BACKGROUND_FILEPATH "assets/sprites/sky.png"
-#define GAMEOVER_BACKGROUND_FILEPATH "assets/sprites/sky.png"
+const std::string GAME_BACKGROUND_FILEPATH = "assets/sprites/sky.png";
+const std::string GAMEOVER_BACKGROUND_FILEPATH = "assets/sprites/sky.png";
 
-#define LAND_FILEPATH "assets/sprites/land.png"
-#define LAND_MOVEMENT_SPEED 200.0f
+const std::string LAND_FILEPATH = "assets/sprites/land.png";
+const float LAND_MOVEMENT_SPEED = 200.0f;
 
-#define GAME_TITLE_FILEPATH "assets/sprites/title.png"
-#define PLAY_BUTTON_FILEPATH "assets/sprites/PlayButton.png"
+const std::string GAME_TITLE_FILEPATH = "assets/sprites/title.png";
+const std::string PLAY_BUTTON_FILEPATH = "assets/sprites/PlayButton.png";
 
-#define PIPE_UP_FILEPATH "assets/sprites/PipeUp.png"
-#define PIPE_DOWN_FILEPATH "assets/sprites/PipeDown.png"
-#define PIPE_SCORING_FILEPATH "assets/sprites/InvisibleScoringPipe.png"
-#define PIPE_MOVEMENT_SPEED 200.0f
-#define PIPE_SPAWN_FREQUENCY 3.0f
+const std::string PIPE_UP_FILEPATH = "assets/sprites/PipeUp.png";
+const std::string PIPE_DOWN_FILEPATH = "assets/sprites/PipeDown.png";
+const std::string PIPE_SCORING_FILEPATH =
+    "assets/sprites/InvisibleScoringPipe.png";
+const float PIPE_MOVEMENT_SPEED = 200.0f;
+const float PIPE_SPAWN_FREQUENCY = 3.0f;
 
-#define BIRD_FRAME_1_FILEPATH "assets/sprites/bird-01.png"
-#define BIRD_FRAME_2_FILEPATH "assets/sprites/bird-02.png"
-#define BIRD_FRAME_3_FILEPATH "assets/sprites/bird-03.png"
-#define BIRD_FRAME_4_FILEPATH "assets/sprites/bird-04.png"
-#define BIRD_ANIMATION_DURATION 0.4f
-#define BIRD_ROTATION_SPEED 100.0f
-#define BIRD_STATE_STILL 1
-#define BIRD_STATE_FALLING 2
-#define BIRD_STATE_FLYING 3
-#define BIRD_FLYING_SPEED 350.0f
-#define FLYING_DURATION 0.25f
-#define GRAVITY 350.0f
+const std::string BIRD_FRAME_1_FILEPATH = "assets/sprites/bird-01.png";
+const std::string BIRD_FRAME_2_FILEPATH = "assets/sprites/bird-02.png";
+const std::string BIRD_FRAME_3_FILEPATH = "assets/sprites/bird-03.png";
+const std::string BIRD_FRAME_4_FILEPATH = "assets/sprites/bird-04.png";
+const float BIRD_ANIMATION_DURATION = 0.4f;
+const float BIRD_ROTATION_SPEED = 100.0f;
+const int BIRD_STATE_STILL = 1;
+const int BIRD_STATE_FALLING = 2;
+const int BIRD_STATE_FLYING = 3;
+const float BIRD_FLYING_SPEED = 350.0f;
+const float FLYING_DURATION = 0.25f;
+const float GRAVITY = 350.0f;
 
-#define FLASH_SPEED 1500.0f
+const float FLASH_SPEED = 1500.0f;
 
-#define FLAPPY_FONT_FILEPATH "assets/fonts/FlappyFont.ttf"
+const std::string FLAPPY_FONT_FILEPATH = "assets/fonts/FlappyFont.ttf";
 
-#define GAME_OVER_TITLE_FILEPATH "assets/sprites/Game-Over-Title.png"
-#define GAME_OVER_BODY_FILEPATH "assets/sprites/Game-Over-Body.png"
+const std::string GAME_OVER_TITLE_FILEPATH =
+    "assets/sprites/Game-Over-Title.png";
+const std::string GAME_OVER_BODY_FILEPATH = "assets/sprites/Game-Over-Body.png";
 
-#define HIGHSCORE_FILEPATH "assets/HighScore.txt"
+const std::string HIGHSCORE_FILEPATH = "assets/HighScore.txt";
 
 enum GameStates { eReady, ePlaying, eGameOver }; // enum GameStates
 
-#define TIME_BEFORE_GAME_OVER_APPEARS 1.5f
+const float TIME_BEFORE_GAME_OVER_APPEARS = 1.5f;
 
 #endif // DEFINITIONS_H
