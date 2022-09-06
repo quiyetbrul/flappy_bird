dce7925a000fd76a2b457a8fa59368a142335547
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2022-09-06 13:15:12 -0600

    Update Definitions.h

--- src/Utils/Definitions.h
+++ src/Utils/Definitions.h
@@ -10,29 +10,29 @@ const int SCREEN_HEIGHT = 1024;
 
 const float SPLASH_STATE_SHOW_TIME = 0.0f;
 const std::string SPLASH_SCENE_BACKGROUND_FILEPATH =
-    "assets/sprites/SplashBackground.png";
+    "../assets/sprites/SplashBackground.png";
 
-const std::string MENU_BACKGROUND_FILEPATH =  "assets/sprites/sky.png";
-const std::string GAME_BACKGROUND_FILEPATH = "assets/sprites/sky.png";
-const std::string GAMEOVER_BACKGROUND_FILEPATH = "assets/sprites/sky.png";
+const std::string MENU_BACKGROUND_FILEPATH =  "../assets/sprites/sky.png";
+const std::string GAME_BACKGROUND_FILEPATH = "../assets/sprites/sky.png";
+const std::string GAMEOVER_BACKGROUND_FILEPATH = "../assets/sprites/sky.png";
 
-const std::string LAND_FILEPATH = "assets/sprites/land.png";
+const std::string LAND_FILEPATH = "../assets/sprites/land.png";
 const float LAND_MOVEMENT_SPEED = 200.0f;
 
-const std::string GAME_TITLE_FILEPATH = "assets/sprites/title.png";
-const std::string PLAY_BUTTON_FILEPATH = "assets/sprites/PlayButton.png";
+const std::string GAME_TITLE_FILEPATH = "../assets/sprites/title.png";
+const std::string PLAY_BUTTON_FILEPATH = "../assets/sprites/PlayButton.png";
 
-const std::string PIPE_UP_FILEPATH = "assets/sprites/PipeUp.png";
-const std::string PIPE_DOWN_FILEPATH = "assets/sprites/PipeDown.png";
+const std::string PIPE_UP_FILEPATH = "../assets/sprites/PipeUp.png";
+const std::string PIPE_DOWN_FILEPATH = "../assets/sprites/PipeDown.png";
 const std::string PIPE_SCORING_FILEPATH =
-    "assets/sprites/InvisibleScoringPipe.png";
+    "../assets/sprites/InvisibleScoringPipe.png";
 const float PIPE_MOVEMENT_SPEED = 200.0f;
 const float PIPE_SPAWN_FREQUENCY = 3.0f;
 
-const std::string BIRD_FRAME_1_FILEPATH = "assets/sprites/bird-01.png";
-const std::string BIRD_FRAME_2_FILEPATH = "assets/sprites/bird-02.png";
-const std::string BIRD_FRAME_3_FILEPATH = "assets/sprites/bird-03.png";
-const std::string BIRD_FRAME_4_FILEPATH = "assets/sprites/bird-04.png";
+const std::string BIRD_FRAME_1_FILEPATH = "../assets/sprites/bird-01.png";
+const std::string BIRD_FRAME_2_FILEPATH = "../assets/sprites/bird-02.png";
+const std::string BIRD_FRAME_3_FILEPATH = "../assets/sprites/bird-03.png";
+const std::string BIRD_FRAME_4_FILEPATH = "../assets/sprites/bird-04.png";
 const float BIRD_ANIMATION_DURATION = 0.4f;
 const float BIRD_ROTATION_SPEED = 100.0f;
 const int BIRD_STATE_STILL = 1;
@@ -44,13 +44,13 @@ const float GRAVITY = 350.0f;
 
 const float FLASH_SPEED = 1500.0f;
 
-const std::string FLAPPY_FONT_FILEPATH = "assets/fonts/FlappyFont.ttf";
+const std::string FLAPPY_FONT_FILEPATH = "../assets/fonts/FlappyFont.ttf";
 
 const std::string GAME_OVER_TITLE_FILEPATH =
-    "assets/sprites/Game-Over-Title.png";
-const std::string GAME_OVER_BODY_FILEPATH = "assets/sprites/Game-Over-Body.png";
+    "../assets/sprites/Game-Over-Title.png";
+const std::string GAME_OVER_BODY_FILEPATH = "../assets/sprites/Game-Over-Body.png";
 
-const std::string HIGHSCORE_FILEPATH = "assets/HighScore.txt";
+const std::string HIGHSCORE_FILEPATH = "../assets/HighScore.txt";
 
 enum GameStates { eReady, ePlaying, eGameOver }; // enum GameStates
 
