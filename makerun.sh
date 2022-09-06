44b7b9217e11715d6c9cf072102553860b416d39
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2022-09-06 14:35:26 -0600

    Update makerun.sh

--- makerun.sh
+++ makerun.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
-cmake --build build --target clean
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  rm -rf build
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  cmake --build build --target clean
+fi
 cmake -S . -B build/
 cd build && make && ./flappy_bird
