06e69267005b7fe434552c8fa5f658970f7566b7
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2022-09-06 17:35:49 -0600

    build update for itch

--- makerun.sh
+++ makerun.sh
@@ -1,9 +1,4 @@
 #! /bin/sh
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  rm -rf build
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  cmake --build build --target clean
-fi
 cmake -S . -B build/
 cd build && make && ./flappy_bird
